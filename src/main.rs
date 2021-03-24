e753d369214864e22c4378e086e2505d2c201318
Joe Wells <joe.wells@depop.com>
2021-03-24 18:26:37 +0000

    clean up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
 use std::env;
 use std::fs::File;
-
-use reqwest::Error;
-
-use crate::soundcloud::{DownloadTrackResponse, TrackInfo};
 use std::io::Write;
+
+use bytes::Bytes;
+
+use soundcloud::{DownloadTrackResponse, TrackInfo};
 
 mod soundcloud;
 
 fn main() {
     let url: String = env::args().nth(1).expect("please supply an argument");
 
-    let response: Result<soundcloud::TrackInfo, Error> = soundcloud::resolve_track_info(url);
+    let track_info: TrackInfo = soundcloud::resolve_track_info(url).expect("Failed to resolve track info");
 
-    match response {
-        Ok(track_info) => if track_info.downloadable { download_track(track_info) } else { println!("Track is not downloadable") },
-        Err(error) => println!("{}", error.to_string())
+    if track_info.downloadable {
+        let raw_track: Bytes = download_raw_track(&track_info).expect("Failed to download raw track");
+        save_track_locally(raw_track, track_info.permalink).expect("Failed to save raw track");
+    } else {
+        panic!("Track is not downloadable")
     }
 }
 
-fn download_track(track_info: TrackInfo) {
-    let response: DownloadTrackResponse = soundcloud::get_download_link(track_info.id).unwrap();
-    let bytes = soundcloud::get_bytes(response.stream_url).unwrap();
-    let mut buffer = File::create(format!("{}.mp3", track_info.permalink)).unwrap();
-    buffer.write_all(&bytes[..]).unwrap()
+fn download_raw_track(track_info: &TrackInfo) -> reqwest::Result<Bytes> {
+    let download_response: DownloadTrackResponse = soundcloud::get_download_link(track_info.id)?;
+    soundcloud::get_bytes(download_response.stream_url)
+}
+
+fn save_track_locally(bytes: Bytes, name: String) -> std::io::Result<()> {
+    let mut buffer: File = File::create(format!("{}.mp3", name))?;
+    buffer.write_all(&bytes[..])
 }
 
